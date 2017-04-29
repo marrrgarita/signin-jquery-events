9185b56eb538dfb1d019c427c9007736edf5d3bd
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-29 15:26:30 -0400

    Adding js to make it more interactive

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+  $('.signin').on('click', function(){
+    $('.modal').fadeIn();
+  })
+
+  $('.modal').on('click', function(){
+    $('.modal').fadeOut();
+  })
+
+  $('.close').on('click', function(){
+    $('.modal').fadeOut();
+  })
+
+  $('.submit').on('click', function(){
+    $('input').addClass('error');
+  })
+
+  $('input').on('mouseenter', function(){
+    $(this).removeClass('error');
+  })
+
+  $('input').on('mouseleave', function(){
+    $(this).addClass('error');
+  })
+
+
+});
+
+// (Bonus) Allow the user to click the .modal to close itself
+// Tricky, consider what happens when two elements that are overlapping are both waiting for a click
